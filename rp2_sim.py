c588db5323a2bf4703ca04287944f6af61df9cec
Gerardo Muñoz <gmunoz.udistrital.edu.co@gmail.com>
2022-09-30 21:34:45 -0500

    Lee las instrucciones y las almacena en la SM

--- rp2_sim.py
+++ rp2_sim.py
@@ -1,20 +1,83 @@
+def asm_pio(*args, **kwargs):
+    def decorador(programa):
+        print("Inside decorator")
+        def compilador():
+            print("Inside inner function")
+            print("I like", kwargs) 
+            programa()
+            return None
+        return compilador
+    return decorador
+
+
+
+#lista_instr=[]
+def decorador_instr(fun_inst):
+    # global 
+    def decoracion_instr(self,*args, **kwargs):
+        #global lista_instr
+        instr1=fun_inst(self,*args, **kwargs)
+        #lista_instr.append(instr1)
+        return None #instr1
+    return decoracion_instr
 
 pins='pins'
 
 class PIO():
-    OUT_LOW=0
+    OUT_LOW='PIO.OUT_LOW'
     
 
 class StateMachine:
-  def __init__(self, smnum, prog, freq=125000000, **kwargs):
-      pass
+  def __init__(self, id_, program, freq=125000000, **kwargs):
+        global sm_iniciandoce,fsms
+        sm_iniciandoce=self
+        print('StateMachine.__init__',id_, program, freq, kwargs)
+        self.lista_instr=[]
+        program()
+        print('lista_instr',self.lista_instr)
+        sm_iniciandoce=None
+        fsms[id_]=self
+        pass
       
+#   def add_instr(self):
+#      self.lista_instr
+        
   def active(self, x=None):
        pass
+fsms=[None]*8
+
+sm_iniciandoce=None
+
+# class Instruc():
+#     def __init__(self):
+#         pass
+
+
+
+# @decorador_instr
+# def nop():
+#     return'nop'
+    
+# @decorador_instr
+# def set():
+#     return'set'
+    
+# #@decorador_instr
+# def wrap_target():
+#     return'wrap_target'
+    
+# @decorador_instr
+# def wrap():
+#     return'wrap'
+    
+
 
 class nop:
+    @decorador_instr
     def __init__(self,*args, **kwargs):
-        print('nop.__init__',args,kwargs)
+        global sm_iniciandoce
+        print(self.__class__.__name__,'nop.__init__',args,kwargs)
+        sm_iniciandoce.lista_instr.append(self)
         pass
      
     def __getitem__(self,name):
@@ -36,13 +99,6 @@ class wrap(nop):
          super().__init__(*args, **kwargs)
          pass 
          
-def asm_pio(*args, **kwargs):
-    print("Inside decorator")
-    def inner(func):
-        print("Inside inner function")
-        print("I like", kwargs['set_init']) 
-        func()
-    return inner
 #@decorator(like = "geeksforgeeks")
 #def my_func():
     #print("Inside actual function")
--- titila_nops.py
+++ titila_nops.py
@@ -6,8 +6,10 @@ from rp2_sim import *
 # Blink state machine program. Blinks LED at 10 Hz (with freq=2000)
 # 2000 Hz / (20 cycles per instruction * 10 instructions) = 10 Hz
 # Single pin (base pin) starts at output and logic low
+print('cero')
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 def blink():
+    print('blink')
     wrap_target()
     set(pins, 1) [19]
     nop()        [19]
@@ -21,15 +23,20 @@ def blink():
     nop()        [19]
     wrap()
 
+print('dos')
+
 # Init state machine with "blink" program
 # (state machine 0, running at 2kHz, base pin is GP25 (LED))
 sm = StateMachine(0, blink, freq=2000, set_base=machine.Pin(25))
 
-# Continually start and stop state machine
-while True:
-    print("Starting state machine...")
-    sm.active(1)
-    time.sleep(1)
-    print("Stopping state machine...")
-    sm.active(0)
-    time.sleep(1)
+# start and stop state machine
+print("Starting state machine...")
+sm.active(1)
+time.sleep(1)
+print("Stopping state machine...")
+sm.active(0)
+time.sleep(1)
+print('uno')
+# print(blink)
+# blink()
+#print(rp2.lista_instr)
